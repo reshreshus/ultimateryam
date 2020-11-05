4b2f3ab81d735fc6ab531f3cab72876b81c6a26e
reshreshus <reshreshus@gmail.com>
2020-11-05 23:24:39 +0300

    hey, Ryam here

---
+++ components/Face.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="face">
+    <img class="face__img" alt="Logo" src="~/assets/middlebook.png">
+    <div>
+      <!-- <Logo /> -->
+      <h1 class="title">
+        Hey, Ryam here
+      </h1>
+      <p>
+        Being weird
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="sass">
+.face
+  &__img
+    height: 20rem
+</style>

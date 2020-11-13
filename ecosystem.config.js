cb1daf9dc063cf5a94700053f5a4a9f462fc65f4
reshreshus <reshreshus@gmail.com>
2020-11-13 15:53:59 +0300

    .

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,6 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'ultimate ryam',
+      port: 3333,
       exec_mode: 'cluster',
       instances: 'max', // Or a number of instances
       script: './node_modules/nuxt/bin/nuxt.js',
